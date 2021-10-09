dd7ac83e899ad927005539d6910a76026b9ee220
TTOzzi <devttozzi@gmail.com>
2021-10-09 11:57:02 +0900

    로그인 화면 UI 구현

---
+++ Archive/Onboarding/LoginReactor.swift
@@ -0,0 +1,33 @@
+//
+//  LoginReactor.swift
+//  Archive
+//
+//  Created by TTOzzi on 2021/10/02.
+//
+
+import ReactorKit
+import RxSwift
+
+final class LoginReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
---
+++ Archive/Onboarding/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Archive
+//
+//  Created by TTOzzi on 2021/10/02.
+//
+
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class LoginViewController: UIViewController, View {
+    var disposeBag = DisposeBag()
+    
+    func bind(reactor: LoginReactor) {
+        
+    }
+}
