1e0aa85154991c907381069da0f2011cf58014fb
TTOzzi <devttozzi@gmail.com>
2021-10-14 18:00:09 +0900

    rootViewController 설정 코드 추가
    rootViewController를 로그인 화면으로 설정
    불필요 코드 제거

--- Archive/SceneDelegate.swift
+++ Archive/SceneDelegate.swift
@@ -10,4 +10,14 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let signInViewController = UIStoryboard(name: "Onboarding", bundle: nil)
+            .instantiateViewController(withIdentifier: "SignInViewController")
+        window.rootViewController = signInViewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
 }
