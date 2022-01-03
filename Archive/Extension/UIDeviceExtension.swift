f28601e92747b71507a4b8feea9bf65efc57462a
LEE HANWE <mobiledev@kakao.com>
2022-01-03 14:13:48 +0900

    사진 등록시 커버 이미지 틀어지는 버그 수정
    사진 등록시 커버 이미지 틀어지는 버그 수정

--- Archive/Extension/UIDeviceExtension.swift
+++ Archive/Extension/UIDeviceExtension.swift
@@ -9,11 +9,8 @@ import UIKit
 
 extension UIDevice {
     var hasNotch: Bool {
-        guard #available(iOS 11.0, *), let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return false }
-        if UIDevice.current.orientation.isPortrait {
-            return window.safeAreaInsets.top >= 44
-        } else {
-            return window.safeAreaInsets.left > 0 || window.safeAreaInsets.right > 0
-        }
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        let bottom = window?.safeAreaInsets.bottom ?? 0
+        return bottom > 0
     }
 }
